31315b9f9d10e7f6f186bf9aa882fa9faa88917f
wanggang <wanggang@dyrs.com.cn>
2022-09-14 11:34:23 +0800

    feat(@123456): 功能描述
    undefined

--- src/views/reactive/util.js
+++ src/views/reactive/util.js
@@ -11,3 +11,27 @@ export const def = (obj, key, value, enumerable) =>
     writable: true,
     configurable: true
   });
+
+/**
+ * 把一个形如'data.a.b.c'的字符串路径所表示的值，从真实的data对象中取出来
+ * 使用函数柯里化的技巧
+ */
+export const parsePath = (path) => {
+  // 判断path是否正确
+  if (/[^\w.$]/.test(path)) return;
+  const segments = path.split('.');
+  return function (obj) {
+    // 两种写法
+    /* let key = ''
+          while (key = segments.shift()) {
+              // 判断obj是否正确
+              if (!obj) return;
+              obj = obj[key]
+          } */
+    for (let i = 0; i < segments.length; i++) {
+      if (!obj) return;
+      obj = obj[segments[i]];
+    }
+    return obj;
+  };
+};
